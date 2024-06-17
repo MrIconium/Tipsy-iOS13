265426f09841a40fe7b7cbc624c1972d663368ec
Mehmet Emrah Konya <mehmetemrahk@gmail.com>
2024-06-17 20:53:13 +0300

    Apply solution

---
+++ Tipsy/ResultViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Mehmet Emrah Konya on 17.06.2024.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
